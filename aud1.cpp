a83dc3afb035e5885b27c5f32a8f71145fbb07b8
unknown <hyozan@gmail.com>
2020-05-25 10:19:05 -0700

    lousy reverb and delay, better filter

--- aud1.cpp
+++ aud1.cpp
@@ -10,9 +10,9 @@ using namespace std;
 
 const int noteA0 = 0;
 const int noteA4 = 48;
-float notes[] = {27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, };
+//float notes[] = {27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, };
 
-unsigned int frequency = 44100;
+const int frequency = 44100;
 unsigned int sampleFrequency = 0;
 unsigned int audioBufferSize = 0;
 unsigned int outputAudioBufferSize = 0;
@@ -22,6 +22,14 @@ const unsigned short sampleRange = 32767;
 int gate;
 
 const float envelopePopThreshold = 0.003;
+
+
+float noteFreqency(float a) {
+    return 27.5f * pow(2.0f, (float)a);
+}
+float noteFreqency(int a, int halfSteps) {
+    return noteFreqency((float)a + (float)halfSteps / 12.0f);
+}
 
 struct Envelope {
     float a, d, s, r;
@@ -90,72 +98,326 @@ struct Envelope {
 
 const float fade = 0.05;
 
-struct Frequency {
+struct Note {
     float glide;
-    float freq;
-    float freq0;
+    float note;
+    float note0;
     float t;
     void generate(float * samples, int count) {
-        if (freq != freq0 && t >= 1) {
+        if (note != note0 && t >= 1) {
             t = 0;
         }
         for (int i = 0; i < count; i++) {
-            float f = (freq * t + freq0 * (1 - t));
+            float n = (note * t + note0 * (1 - t));
             t += 1 / (float)frequency / glide;
             if (t >= 1) {
-                t = 1; freq0 = freq;
+                t = 1;
+                note0 = note;
             }
-            samples[i] = f;
+            samples[i] = n;
         }
     }
 };
 
-struct Freq2Signal {
+#define wrap(t) t = t > 1.0f ? 0.0f : t
+
+struct Frequency {
+    float t;
+    void process(float * samples, int count) {
+        for (int i = 0; i < count; i++) {
+            samples[i] = noteFreqency(samples[i]);
+            t += 1 / (float)samples[i];
+        }
+        wrap(t);
+    }
+};
+
+SDL_Renderer * renderer;
+
+int graph[400];
+
+void renderState(float * samples, int count, float min = -1.0f, float max = 1.0f) {
+    int width = count < 400 ? count : 400;
+    float range = max - min;
+    for (int i = 0; i < width; i++) {
+        graph[i] = 400 - (samples[i] - min) / range * 400;
+    }
+}
+
+// Generates waveforms which will bend each sample by sample frequency.
+struct ModulatingSignal {
     float sine;
-    float tri;
+    float saw;
     float square;
     float noise;
     float t;
     void process(float * samples, int count) {
         for (int i=0; i<count; i++) {
             float h = sin(2.0 * M_PI * t) * sine;
-            h += (2.0 * t - 1.0) * tri;
+            h += (2.0 * t - 1.0) * saw;
             h += (t < 0.5 ? -1 : 1) * square;
             h += ((float)rand() / (float)RAND_MAX * 2.0 - 1.0) * noise;
             t += samples[i] / frequency;
             samples[i] = h;
-            t = t > 1 ? 0 : t; // fmod would be more accurate but generates artifact frequencies
+            //wrap(t);
+            t = fmod(t, 1.0f);
         }
     }
 };
 
 
-struct LowPassFilter {
-    float smooth;
-    float beta;
+// Waveform will not change until period is over. 
+// Period is snapped to sample count.
+struct FixedSignal {
+    float sine;
+    float tri;
+    float square;
+    float noise;
+    int wavelength;
+    int cursor;
+    void process(float * samples, int count) {
+        for (int i = 0; i < count; i++) {
+            if (cursor >= wavelength - 1) {
+                wavelength = frequency / samples[i];
+                cursor = 0;
+            }
+            float t = (float)cursor / (float)(wavelength - 1);
+            float h = sin(2.0 * M_PI * t) * sine;
+            h += (2.0 * t - 1.0) * tri;
+            h += (t < 0.5 ? -1 : 1) * square;
+            h += ((float)rand() / (float)RAND_MAX * 2.0 - 1.0) * noise;
+            samples[i] = h;
+            cursor++;
+        }
+    }
+};
+
+struct LFO {
+    float amplitude;
+    float period;
+    float t;
+    void process(float * samples, int count) {
+        for (int i = 0; i < count; i++) {
+            samples[i] += sin(2.0 * M_PI * t) * amplitude;
+            t += 1 / (float)frequency / period;
+            wrap(t);
+        }
+    }
+};
+
+const int window = 7;
+
+struct BasicFilter {
+    float last[window];
+    int next;
+    float avg;
+    float sum;
+
     void process(float * stream, int count) {
         for (int i = 0; i < count; i++) {
-            smooth = stream[i] = smooth - (beta * (smooth - stream[i]));
+            sum += stream[i];
+            sum -= last[next];
+            last[next] = stream[i];
+            stream[i] = sum / (float)window;
+            ++next %= window;
         }
     }
 };
 
-Frequency freqGen{0.001, 440, 440};
-Freq2Signal freq2Sig{ 1, 0, 0, 0 };
+const int delayBufferSize = frequency;
+
+struct Delay {
+    float delay;
+    float decay;
+    int read;
+    float buffer[delayBufferSize];
+
+    void process(float * samples, int count) {
+        int delay = this->delay * (float)frequency;
+        delay = delay > (frequency - 1) ? (frequency - 1) : delay;
+        for (int i = 0; i < count; i++) {
+            samples[i] += buffer[read];
+            buffer[(read + delay) % delayBufferSize] = samples[i] * decay;
+            read++;
+            read %= delayBufferSize;
+        }
+    }
+};
+
+// explicit delay in sample count
+struct ExplicitDelay {
+    int delay;
+    float decay;
+    int read;
+    float buffer[delayBufferSize];
+    void process(float * samples, int count) {
+        delay = delay > (frequency - 1) ? (frequency - 1) : delay;
+        for (int i = 0; i < count; i++) {
+            samples[i] += buffer[read];
+            buffer[(read + delay) % delayBufferSize] = samples[i] * decay;
+            read++;
+            read %= delayBufferSize;
+        }
+    }
+};
+
+int baseDelay = 441;
+
+struct Reverb {
+    ExplicitDelay delays[10];
+    Reverb() {
+        delays[0].delay = baseDelay + 29; delays[0].decay = 0.1;
+        delays[1].delay = baseDelay + 601; delays[1].decay = 0.2;
+        delays[2].delay = baseDelay + 1291; delays[2].decay = 0.4;
+        delays[3].delay = baseDelay + 2053; delays[3].decay = 0.6;
+        delays[4].delay = baseDelay + 2819; delays[4].decay = 0.7;
+        delays[5].delay = baseDelay + 3643; delays[5].decay = 0.8;
+        delays[6].delay = baseDelay + 4493; delays[6].decay = 0.7;
+        delays[7].delay = baseDelay + 5387; delays[7].decay = 0.6;
+        delays[8].delay = baseDelay + 6221; delays[8].decay = 0.2;
+        delays[9].delay = baseDelay + 7103; delays[9].decay = 0.1;
+    }
+
+    void process(float * samples, int count) {
+        for (int d = 0; d < 4; d++) {
+            delays[d].process(samples, count);
+        }
+    }
+};
+
+struct CombinedReverb {
+    float decay;
+    int delays[10];
+    int read;
+    float buffer[delayBufferSize];
+    CombinedReverb() {
+        decay = 0.1;
+        delays[0] = 29;
+        delays[0] = 601;
+        delays[1] = 1291;
+        delays[2] = 2053;
+        delays[3] = 2819;
+        delays[4] = 3643;
+        delays[5] = 4493;
+        delays[6] = 5387;
+        delays[7] = 6221;
+        delays[8] = 7103;
+    }
+
+    void process(float * samples, int count) {
+       for (int i = 0; i < count; i++) {
+            samples[i] += buffer[read];
+            for (int j = 4; j < 9; j++) {
+                buffer[(read + delays[j]) % delayBufferSize] += samples[i] * decay;
+            }
+            read++;
+            read %= delayBufferSize;
+        }
+    }
+};
+
+/*
+      2      3      5      7     11     13     17     19     23     29 
+     31     37     41     43     47     53     59     61     67     71 
+     73     79     83     89     97    101    103    107    109    113 
+    127    131    137    139    149    151    157    163    167    173 
+    179    181    191    193    197    199    211    223    227    229 
+    233    239    241    251    257    263    269    271    277    281 
+    283    293    307    311    313    317    331    337    347    349 
+    353    359    367    373    379    383    389    397    401    409 
+    419    421    431    433    439    443    449    457    461    463 
+    467    479    487    491    499    503    509    521    523    541 
+    547    557    563    569    571    577    587    593    599    601 
+    607    613    617    619    631    641    643    647    653    659 
+    661    673    677    683    691    701    709    719    727    733 
+    739    743    751    757    761    769    773    787    797    809 
+    811    821    823    827    829    839    853    857    859    863 
+    877    881    883    887    907    911    919    929    937    941 
+    947    953    967    971    977    983    991    997   1009   1013 
+   1019   1021   1031   1033   1039   1049   1051   1061   1063   1069 
+   1087   1091   1093   1097   1103   1109   1117   1123   1129   1151 
+   1153   1163   1171   1181   1187   1193   1201   1213   1217   1223 
+   1229   1231   1237   1249   1259   1277   1279   1283   1289   1291 
+   1297   1301   1303   1307   1319   1321   1327   1361   1367   1373 
+   1381   1399   1409   1423   1427   1429   1433   1439   1447   1451 
+   1453   1459   1471   1481   1483   1487   1489   1493   1499   1511 
+   1523   1531   1543   1549   1553   1559   1567   1571   1579   1583 
+   1597   1601   1607   1609   1613   1619   1621   1627   1637   1657 
+   1663   1667   1669   1693   1697   1699   1709   1721   1723   1733 
+   1741   1747   1753   1759   1777   1783   1787   1789   1801   1811 
+   1823   1831   1847   1861   1867   1871   1873   1877   1879   1889 
+   1901   1907   1913   1931   1933   1949   1951   1973   1979   1987 
+   1993   1997   1999   2003   2011   2017   2027   2029   2039   2053 
+   2063   2069   2081   2083   2087   2089   2099   2111   2113   2129 
+   2131   2137   2141   2143   2153   2161   2179   2203   2207   2213 
+   2221   2237   2239   2243   2251   2267   2269   2273   2281   2287 
+   2293   2297   2309   2311   2333   2339   2341   2347   2351   2357 
+   2371   2377   2381   2383   2389   2393   2399   2411   2417   2423 
+   2437   2441   2447   2459   2467   2473   2477   2503   2521   2531 
+   2539   2543   2549   2551   2557   2579   2591   2593   2609   2617 
+   2621   2633   2647   2657   2659   2663   2671   2677   2683   2687 
+   2689   2693   2699   2707   2711   2713   2719   2729   2731   2741 
+   2749   2753   2767   2777   2789   2791   2797   2801   2803   2819 
+   2833   2837   2843   2851   2857   2861   2879   2887   2897   2903 
+   2909   2917   2927   2939   2953   2957   2963   2969   2971   2999 
+   3001   3011   3019   3023   3037   3041   3049   3061   3067   3079 
+   3083   3089   3109   3119   3121   3137   3163   3167   3169   3181 
+   3187   3191   3203   3209   3217   3221   3229   3251   3253   3257 
+   3259   3271   3299   3301   3307   3313   3319   3323   3329   3331 
+   3343   3347   3359   3361   3371   3373   3389   3391   3407   3413 
+   3433   3449   3457   3461   3463   3467   3469   3491   3499   3511 
+   3517   3527   3529   3533   3539   3541   3547   3557   3559   3571 
+   3581   3583   3593   3607   3613   3617   3623   3631   3637   3643 
+   3659   3671   3673   3677   3691   3697   3701   3709   3719   3727 
+   3733   3739   3761   3767   3769   3779   3793   3797   3803   3821 
+   3823   3833   3847   3851   3853   3863   3877   3881   3889   3907 
+ */
+
+
+
+Note noteGen{ 0.001, 4, 4 };
+Frequency noteFreq;
+ModulatingSignal modsig { 0, 1, 0, 0 };
+FixedSignal fixedsig { 0, 1, 0, 0 };
 Envelope env{0, 0, 1, 0, 0, 0, 4};
-LowPassFilter filter{ 0, 0.025 };
+BasicFilter basicFilter;
+Delay delay{ 0.5, 0.5 };
+Reverb reverb;
+CombinedReverb combinedReverb;
+
+ExplicitDelay ed1{ 229, 0.5 };
+ExplicitDelay ed2{ 1069, 0.5 };
+ExplicitDelay ed3{ 3181, 0.5 };
+ExplicitDelay ed4{ 6053, 0.5 };
+ExplicitDelay ed5{ 7919, 0.5 };
+LFO lfo{ 0.01, 0.5 };
+
+int mode = 0;
+
+#define FLOATSTREAM (float*)stream, len / 4
 
 void fillAudio(void *unused, Uint8 *stream, int len) {
-    freqGen.generate((float*)stream, len / 4);
-    freq2Sig.process((float*)stream, len / 4);
-    //filter.process((float*)stream, len / 4);
-    env.process((float*)stream, len/4, gate);
+    noteGen.generate(FLOATSTREAM);
+    lfo.process(FLOATSTREAM);
+    //renderState((float*)stream, len / 4, noteGen.note-0.1, noteGen.note+0.1);
+    noteFreq.process(FLOATSTREAM);
+    modsig.process(FLOATSTREAM);
+    env.process(FLOATSTREAM, gate);
+    if (mode == 0) {
+        reverb.process(FLOATSTREAM);
+    }
+    else if (mode == 1) {
+        delay.process(FLOATSTREAM);
+    }
+    basicFilter.process(FLOATSTREAM);
+    renderState((float*)stream, len);
 }
 
 void noteOn(SDL_Event & e, int note) {
     if (e.key.repeat) return;
     gate++;
-    freqGen.freq = notes[note];
+    //freqGen.note = (float)note / 12.0f;
+    noteGen.note = (float)note / 12.0f;
 
     env.trigger();
 }
@@ -193,10 +455,11 @@ int main(int argc, char *argv[]) {
 
     SDL_Window * window = SDL_CreateWindow(
         "audio", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 400, 400, 0);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
 
     SDL_PauseAudio(0);
 
-    int octave = noteA4+12;
+    int octave = noteA4;
 
     bool running = true;
     SDL_Event event;
@@ -222,6 +485,7 @@ int main(int argc, char *argv[]) {
                 case SDLK_F4: if (event.key.keysym.mod & KMOD_ALT) running = false; break;
                 case SDLK_PAGEDOWN: octave = octave ? octave-12 : octave; break;
                 case SDLK_PAGEUP: octave = octave < (12*7) ? octave+12 : octave; break;
+                case SDLK_SPACE: ++mode %= 3; break;
                 }
                 break;
             case SDL_KEYUP:
@@ -247,6 +511,12 @@ int main(int argc, char *argv[]) {
             }
         }
         SDL_Delay(10);
+        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+        SDL_RenderClear(renderer);
+        SDL_SetRenderDrawColor(renderer, 255, 255, 255, 255);
+        for (int i = 0; i < 400; i++)
+            SDL_RenderDrawLine(renderer, i, 400, i, graph[i]);
+        SDL_RenderPresent(renderer);
     }
     printf("quitting\n");
     SDL_Quit();
---
+++ notes.lua
@@ -0,0 +1,1 @@
+function notefreq(n, hs) return 27.5 * math.pow(2, n + hs/12) end 
