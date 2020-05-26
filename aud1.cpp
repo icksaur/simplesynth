147f9a3550140f70f870d341505172bc2bae61b5
unknown <hyozan@gmail.com>
2020-05-25 23:02:15 -0700

    BasicFilter is back, mouse Y loc to change cutoff

--- aud1.cpp
+++ aud1.cpp
@@ -8,16 +8,10 @@
 
 using namespace std;
 
-const int noteA0 = 0;
 const int noteA4 = 48;
-//float notes[] = {27.50, 29.14, 30.87, 32.70, 34.65, 36.71, 38.89, 41.20, 43.65, 46.25, 49.00, 51.91, 55.00, 58.27, 61.74, 65.41, 69.30, 73.42, 77.78, 82.41, 87.31, 92.50, 98.00, 103.83, 110.00, 116.54, 123.47, 130.81, 138.59, 146.83, 155.56, 164.81, 174.61, 185.00, 196.00, 207.65, 220.00, 233.08, 246.94, 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88, 523.25, 554.37, 587.33, 622.25, 659.26, 698.46, 739.99, 783.99, 830.61, 880.00, 932.33, 987.77, 1046.50, 1108.73, 1174.66, 1244.51, 1318.51, 1396.91, 1479.98, 1567.98, 1661.22, 1760.00, 1864.66, 1975.53, 2093.00, 2217.46, 2349.32, 2489.02, 2637.02, 2793.83, 2959.96, 3135.96, 3322.44, 3520.00, 3729.31, 3951.07, 4186.01, 4434.92, 4698.64, 4978.03, 5274.04, 5587.65, 5919.91, 6271.93, 6644.88, 7040.00, };
-
 const int frequency = 44100;
 unsigned int sampleFrequency = 0;
 unsigned int audioBufferSize = 0;
-unsigned int outputAudioBufferSize = 0;
-
-const unsigned short sampleRange = 32767;
 
 int gate;
 
@@ -207,6 +201,17 @@ struct LFO {
 const int window = 7;
 
 struct BasicFilter {
+    float k;
+    float r;
+    void process(float * stream, int count) {
+        for (int i = 0; i < count; i++) {
+            stream[i] = (stream[i] - r) * k + r;
+            r = stream[i];
+        }
+    }
+};
+
+struct WindowFilter {
     float last[window];
     int next;
     float avg;
@@ -327,7 +332,8 @@ Frequency noteFreq;
 ModulatingSignal modsig { 0, 1, 0, 0 };
 FixedSignal fixedsig { 0, 1, 0, 0 };
 Envelope env{0, 0, 1, 0, 0, 0, 4};
-BasicFilter basicFilter;
+WindowFilter windowFilter;
+BasicFilter basicFilter{ 0.5 };
 Delay delay{ 0.5, 0.5 };
 Reverb reverb;
 CombinedReverb combinedReverb;
@@ -350,20 +356,18 @@ void fillAudio(void *unused, Uint8 *stream, int len) {
     noteFreq.process(FLOATSTREAM);
     modsig.process(FLOATSTREAM);
     env.process(FLOATSTREAM, gate);
-    if (mode == 0) {
-        combinedReverb.process(FLOATSTREAM);
-    }
-    else if (mode == 1) {
-        reverb.process(FLOATSTREAM);
-    }
-    basicFilter.process(FLOATSTREAM);
+    //combinedReverb.process(FLOATSTREAM);
+    delay.process(FLOATSTREAM);
+    if (mode == 0)
+        basicFilter.process(FLOATSTREAM);
+    else
+        windowFilter.process(FLOATSTREAM);
     renderState((float*)stream, len);
 }
 
 void noteOn(SDL_Event & e, int note) {
     if (e.key.repeat) return;
     gate++;
-    //freqGen.note = (float)note / 12.0f;
     noteGen.note = (float)note / 12.0f;
 
     env.trigger();
@@ -455,6 +459,9 @@ int main(int argc, char *argv[]) {
             case SDL_QUIT:
             running = false;
             break;
+            case SDL_MOUSEMOTION:
+                basicFilter.k = (float)event.motion.y / (float)400;
+                break;
             }
         }
         SDL_Delay(10);
